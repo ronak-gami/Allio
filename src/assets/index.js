d181d1eb68fe495cc43f700ddffd63a0c1b27e63
ronak-gami <ronakg.itpath@gmail.com>
2025-07-22 14:50:12 +0530

    added: tab styles & icons

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,6 +1,10 @@
 export const IMAGES = {
-    First: require('./images/firstScreen.png'),
-    SecondOnboarding: require('./images/onboardingSecond.png'),
-    OnboardingThree: require('./images/onboardingThree.png'),
-
+  First: require('./images/firstScreen.png'),
+  SecondOnboarding: require('./images/onboardingSecond.png'),
+  OnboardingThree: require('./images/onboardingThree.png'),
+  Home: require('./images/home.png'),
+  ImageMedia: require('./images/image-media.png'),
+  More: require('./images/more.png'),
+  ScanQR: require('./images/scanQR.png'),
+  VideoMedia: require('./images/video-media.png'),
 };
