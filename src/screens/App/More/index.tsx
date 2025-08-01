bd0dcc2353f0d501603050e9554e8624939fa649
ronak-gami <ronakg.itpath@gmail.com>
2025-08-01 17:05:54 +0530

    fix : package.json & logout loading

--- src/screens/App/More/index.tsx
+++ src/screens/App/More/index.tsx
@@ -1,41 +1,39 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { View } from 'react-native';
 import { Text } from 'react-native-paper';
 import { useDispatch } from 'react-redux';
 import useStyle from './style';
 import Button from '@components/atoms/Button';
-import { logout } from '@redux/slices/AuthSlice';
 import { BottomTabScreenProps } from '@react-navigation/bottom-tabs';
 import { TabParamList } from '@types/navigations';
 import firestore from '@react-native-firebase/firestore';
-import messaging from '@react-native-firebase/messaging';
 import crashlytics from '@react-native-firebase/crashlytics';
 import { getAuth } from '@react-native-firebase/auth';
-import { setStateKey, logout as reduxLogout } from '@redux/slices/AuthSlice';
+import { logout } from '@redux/slices/AuthSlice';
+
 type Props = BottomTabScreenProps<TabParamList, 'More'>;
 
 const More: React.FC<Props> = () => {
-  const [loading, setLoading] = useState<boolean>(false);
   const styles = useStyle();
   const dispatch = useDispatch();
 
   const handleLogout = async () => {
-    setLoading(true);
     try {
-      const auth = getAuth();
-      const currentUser = auth.currentUser;
+      const authInstance = getAuth();
+      const currentUser = authInstance.currentUser;
+
       if (currentUser) {
         await firestore().collection('users').doc(currentUser.uid).update({
-          fcmToken: null,
-          fcmUpdatedAt: null,
+          fcmToken: firestore.FieldValue.delete(),
+          fcmUpdatedAt: firestore.FieldValue.delete(),
         });
-        await auth.signOut();
-        dispatch(reduxLogout());
+
+        await authInstance.signOut();
+        dispatch(logout());
       }
     } catch (error) {
+      console.error('Logout Error:', error);
       crashlytics().recordError(error as Error);
-    } finally {
-      setLoading(false);
     }
   };
 
