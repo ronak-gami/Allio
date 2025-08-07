502fbeb52e59479aaf5d8da8cf69af869f32333a
ayushnathvani <ayushn.itpathsolutions@gmail.com>
2025-08-07 11:09:54 +0530

    more screnn update

--- src/screens/App/More/style.ts
+++ src/screens/App/More/style.ts
@@ -1,42 +1,40 @@
 import { StyleSheet } from 'react-native';
-import { height } from '@utils/helper';
-import { scale } from 'react-native-size-matters';
 import { useTheme } from '@react-navigation/native';
+import { scale } from 'react-native-size-matters';
+import { height } from '@utils/helper';
 
 const useStyle = () => {
   const { colors } = useTheme();
   return StyleSheet.create({
     container: {
       flex: 1,
-      padding: height * 0.02,
       backgroundColor: colors.background,
+      justifyContent: 'center',
+      alignItems: 'center',
     },
     title: {
       fontSize: scale(22),
       fontWeight: 'bold',
       marginBottom: height * 0.02,
       textAlign: 'center',
+      color: colors.text,
     },
     item: {
       paddingVertical: height * 0.025,
-      // paddingHorizontal: height * 0.02,
       borderBottomWidth: 1,
-      borderColor: colors.lightGrey,
+      borderColor: colors.text,
     },
     itemText: {
       fontSize: scale(16),
+      color: colors.text,
     },
-    emptyText: {
-      textAlign: 'center',
-      marginTop: height * 0.2,
-      fontSize: scale(16),
-      color: colors.grey,
-    },
-    center: {
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center',
+    logoutButton: {
+      backgroundColor: colors.primary,
+      paddingHorizontal: scale(20),
+      paddingVertical: scale(10),
+      borderRadius: scale(5),
     },
   });
 };
+
 export default useStyle;
